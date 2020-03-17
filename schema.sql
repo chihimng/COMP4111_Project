b68df4238cf5fd28ee05da6abf165acbab07a2ca
Elliot Ng <chihimng.io@gmail.com>
2020-03-17 10:17:25 +0800

    feat: implement database schema for user and sessions

---
+++ schema.sql
@@ -0,0 +1,20 @@
+create schema if not exists comp4111 collate latin1_swedish_ci;
+
+create table if not exists user
+(
+	username varchar(32) not null
+		primary key,
+	password char(64) null
+);
+
+create table if not exists sessions
+(
+	username varchar(32) not null
+		primary key,
+	access_token varchar(32) not null,
+	constraint sessions_access_token_uindex
+		unique (access_token),
+	constraint sessions_user_username_fk
+		foreign key (username) references user (username)
+);
+
