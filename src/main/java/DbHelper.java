1122cd39aa127054367e4859e003320e3270a4e1
Esmond <esmond9981127@gmail.com>
2020-03-29 17:13:41 +0800

    feat: implement try catch autoclose shortcut in all dbhelper methods

--- src/main/java/DbHelper.java
+++ src/main/java/DbHelper.java
@@ -64,11 +64,7 @@ public class DbHelper {
 
     public String signIn(String username, String password) throws SignInException {
         String token = UUID.randomUUID().toString();
-        Connection conn = null;
-        PreparedStatement stmt = null;
-        try {
-            conn = DriverManager.getConnection(dbUrl);
-            stmt = conn.prepareStatement("INSERT INTO session (username, token) VALUES ((SELECT username FROM user WHERE username = ? AND password = UNHEX(SHA2(CONCAT(?, salt), 256))), ?)");
+        try (Connection conn = DriverManager.getConnection(dbUrl); PreparedStatement stmt = conn.prepareStatement("INSERT INTO session (username, token) VALUES ((SELECT username FROM user WHERE username = ? AND password = UNHEX(SHA2(CONCAT(?, salt), 256))), ?)")){
             stmt.setString(1, username);
             stmt.setString(2, password);
             stmt.setString(3, token);
@@ -88,11 +84,6 @@ public class DbHelper {
             } else {
                 throw new SignInException(e.getMessage());
             }
-        } finally {
-            try {
-                if (stmt != null) stmt.close();
-                if (conn != null) conn.close();
-            } catch (Exception e) {}
         }
     }
 
@@ -109,11 +100,7 @@ public class DbHelper {
     }
 
     public boolean signOut(String token) throws SignOutException {
-        Connection conn = null;
-        PreparedStatement stmt = null;
-        try {
-            conn = DriverManager.getConnection(dbUrl);
-            stmt = conn.prepareStatement("DELETE FROM session WHERE token = ?;");
+        try (Connection conn = DriverManager.getConnection(dbUrl); PreparedStatement stmt = conn.prepareStatement("DELETE FROM session WHERE token = ?;")){
             stmt.setString(1, token);
             if (stmt.executeUpdate() > 0) { // success
                 return true;
@@ -122,12 +109,6 @@ public class DbHelper {
             }
         } catch (SQLException e) {
             throw new SignOutException(e.getMessage());
-        } finally {
-            try {
-                if (stmt != null) stmt.close();
-                if (conn != null) conn.close();
-            } catch (Exception e) {
-            }
         }
     }
 
@@ -143,19 +124,13 @@ public class DbHelper {
     }
 
     public int createBook(Book book) throws CreateBookException {
-        Connection conn = null;
-        PreparedStatement stmt = null;
-        try {
-            conn = DriverManager.getConnection(dbUrl);
-            stmt = conn.prepareStatement("INSERT INTO book (title, author, publisher, year) VALUES (?, ?, ?, ?);");
+        try (Connection conn = DriverManager.getConnection(dbUrl); PreparedStatement stmt = conn.prepareStatement("INSERT INTO book (title, author, publisher, year) VALUES (?, ?, ?, ?);"); PreparedStatement getIdStmt = conn.prepareCall("SELECT LAST_INSERT_ID();")) {
             stmt.setString(1, book.title);
             stmt.setString(2, book.author);
             stmt.setString(3, book.publisher);
             stmt.setInt(4, book.year);
             if (stmt.executeUpdate() > 0) { // success
-                stmt.close();
-                stmt = conn.prepareCall("SELECT LAST_INSERT_ID();");
-                ResultSet rs = stmt.executeQuery();
+                ResultSet rs = getIdStmt.executeQuery();
                 if (rs.next()) {
                     return rs.getInt(1);
                 } else {
@@ -169,12 +144,6 @@ public class DbHelper {
                 throw new CreateBookConflictException("Duplicate found");
             }
             throw new CreateBookException(e.getMessage());
-        } finally {
-            try {
-                if (stmt != null) stmt.close();
-                if (conn != null) conn.close();
-            } catch (Exception e) {
-            }
         }
     }
 
@@ -192,11 +161,7 @@ public class DbHelper {
 
 
     public int findDuplicateBook(Book book) throws FindDuplicateBookException {
-        Connection conn = null;
-        PreparedStatement stmt = null;
-        try {
-            conn = DriverManager.getConnection(dbUrl);
-            stmt = conn.prepareStatement("SELECT * FROM book WHERE title = ? AND author = ? AND publisher = ? AND year = ?");
+        try (Connection conn = DriverManager.getConnection(dbUrl); PreparedStatement stmt = conn.prepareStatement("SELECT * FROM book WHERE title = ? AND author = ? AND publisher = ? AND year = ?");) {
             stmt.setString(1, book.title);
             stmt.setString(2, book.author);
             stmt.setString(3, book.publisher);
@@ -209,12 +174,6 @@ public class DbHelper {
             }
         } catch (SQLException e) {
             throw new FindDuplicateBookException(e.getMessage());
-        } finally {
-            try {
-                if (stmt != null) stmt.close();
-                if (conn != null) conn.close();
-            } catch (Exception e) {
-            }
         }
     }
 
@@ -225,57 +184,53 @@ public class DbHelper {
     }
 
     public List<Book> searchBook(String id, String title, String author, String sort, String order, String limit) throws SearchBookException {
-        Connection conn = null;
-        PreparedStatement stmt = null;
-        try {
-            conn = DriverManager.getConnection(dbUrl);
-            List<String> clauses = new ArrayList<String>();
-            String query = "SELECT * FROM book";
-            if (id != null) {
-                clauses.add("id = ?");
+        List<String> clauses = new ArrayList<String>();
+        String query = "SELECT * FROM book";
+        if (id != null) {
+            clauses.add("id = ?");
+        }
+        if (title != null && !title.isEmpty()) {
+            clauses.add("title LIKE ?");
+        }
+        if (author != null && !author.isEmpty()) {
+            clauses.add("author LIKE ?");
+        }
+        if (!clauses.isEmpty()) {
+            query += " WHERE " + String.join(" AND ", clauses);
+        }
+        if (sort != null && !sort.isEmpty()) {
+            String orderClause = null;
+            if (sort.equals("id")) {
+                orderClause = "ORDER BY id";
+            } else if (sort.equals("title")) {
+                orderClause = "ORDER BY title";
+            } else if (sort.equals("author")) {
+                orderClause = "ORDER BY author";
             }
-            if (title != null && !title.isEmpty()) {
-                clauses.add("title LIKE ?");
-            }
-            if (author != null && !author.isEmpty()) {
-                clauses.add("author LIKE ?");
-            }
-            if (!clauses.isEmpty()) {
-                query += " WHERE " + String.join(" AND ", clauses);
-            }
-            if (sort != null && !sort.isEmpty()) {
-                String orderClause = null;
-                if (sort.equals("id")) {
-                    orderClause = "ORDER BY id";
-                } else if (sort.equals("title")) {
-                    orderClause = "ORDER BY title";
-                } else if (sort.equals("author")) {
-                    orderClause = "ORDER BY author";
-                }
-                if (orderClause != null) {
-                    if (order != null) {
-                        if (order.contains("asc")) {
-                            orderClause += " ASC";
-                        } else if (order.contains("desc")) {
-                            orderClause += " DESC";
-                        }
+            if (orderClause != null) {
+                if (order != null) {
+                    if (order.contains("asc")) {
+                        orderClause += " ASC";
+                    } else if (order.contains("desc")) {
+                        orderClause += " DESC";
                     }
-                    query += " " + orderClause;
                 }
+                query += " " + orderClause;
             }
-            if (limit != null && !limit.isEmpty()) {
-                Integer limitCount = null;
-                try {
-                    limitCount = Integer.parseInt(limit);
-                } catch (Exception e) {}
-                if (limitCount != null) {
-                    query += " LIMIT " + limitCount.toString();
-                }
+        }
+        if (limit != null && !limit.isEmpty()) {
+            Integer limitCount = null;
+            try {
+                limitCount = Integer.parseInt(limit);
+            } catch (Exception e) {}
+            if (limitCount != null) {
+                query += " LIMIT " + limitCount.toString();
             }
-            query += ";";
-            System.out.println(clauses.toString());
-            System.out.println(query);
-            stmt = conn.prepareStatement(query);
+        }
+        query += ";";
+        System.out.println(clauses.toString());
+        System.out.println(query);
+        try (Connection conn = DriverManager.getConnection(dbUrl); PreparedStatement stmt = conn.prepareStatement(query)){
             int i = 1;
             if (query.contains("id = ?")) {
                 stmt.setInt(i, Integer.parseUnsignedInt(id));
@@ -302,12 +257,6 @@ public class DbHelper {
             return books;
         } catch (SQLException e) {
             throw new SearchBookException(e.getMessage());
-        } finally {
-            try {
-                if (stmt != null) stmt.close();
-                if (conn != null) conn.close();
-            } catch (Exception e) {
-            }
         }
     }
 
