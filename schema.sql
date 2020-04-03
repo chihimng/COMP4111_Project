ed1d36e77f07f0df6cf985579509167db19518b7
Esmond <esmond9981127@gmail.com>
2020-04-03 21:09:30 +0800

    fix: make token in transaction unique

--- schema.sql
+++ schema.sql
@@ -34,12 +34,14 @@ create table session
 
 create table transaction
 (
-	id smallint auto_increment,
+	id MEDIUMINT unsigned auto_increment,
 	last_modified timestamp default CURRENT_TIMESTAMP not null,
 	statement varchar(1000) not null,
 	token char(36) not null,
 	constraint transaction_pk
-		primary key (id)
+		primary key (id),
+    constraint transaction_uindex
+		unique (token)
 );
 
 DELIMITER $$
