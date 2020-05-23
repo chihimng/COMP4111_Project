196d02977a325dc37b729065aed2185ad02c71c1
Elliot Ng <chihimng.io@gmail.com>
2020-05-23 21:42:51 +0800

    feat: reimplement with nio (wip)

--- src/main/java/ApiServer.java
+++ src/main/java/ApiServer.java
@@ -1,7 +1,7 @@
-import org.apache.http.*;
-import org.apache.http.config.SocketConfig;
-import org.apache.http.impl.bootstrap.HttpServer;
-import org.apache.http.impl.bootstrap.ServerBootstrap;
+import org.apache.http.ExceptionLogger;
+import org.apache.http.impl.nio.bootstrap.HttpServer;
+import org.apache.http.impl.nio.bootstrap.ServerBootstrap;
+import org.apache.http.impl.nio.reactor.IOReactorConfig;
 
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
@@ -16,22 +16,22 @@ public class ApiServer {
             System.out.println("Failed to obtain port config from env, reverting to 8080.");
         }
 
-        SocketConfig socketConfig = SocketConfig.custom()
-                .setSoTimeout(15000)
-                .setTcpNoDelay(true)
-                .build();
+        final IOReactorConfig config = IOReactorConfig.custom()
+            .setSoTimeout(15000)
+            .setTcpNoDelay(true)
+            .build();
 
         final HttpServer server = ServerBootstrap.bootstrap()
                 .setListenerPort(port)
                 .setServerInfo("ApiServer/1.0")
-                .setSocketConfig(socketConfig)
+                .setIOReactorConfig(config)
                 .setSslContext(null)
                 .setExceptionLogger(ExceptionLogger.STD_ERR)
                 .registerHandler("/", new HelloWorldRequestHandler())
-                .registerHandler("/BookManagementService/login", new LoginRequestHandler())
-                .registerHandler("/BookManagementService/logout", new LogoutRequestHandler())
-                .registerHandler("/BookManagementService/books*", new BooksRequestHandler())
-                .registerHandler("/BookManagementService/transaction*", new TransactionRequestHandler())
+//                .registerHandler("/BookManagementService/login", new LoginRequestHandler())
+//                .registerHandler("/BookManagementService/logout", new LogoutRequestHandler())
+//                .registerHandler("/BookManagementService/books*", new BooksRequestHandler())
+//                .registerHandler("/BookManagementService/transaction*", new TransactionRequestHandler())
                 .create();
 
         try {
@@ -43,9 +43,12 @@ public class ApiServer {
             return;
         }
 
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-            System.out.println("API Server is shutting down with 5 seconds grace period");
-            server.shutdown(5, TimeUnit.SECONDS);
-        }));
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                System.out.println("API Server is shutting down after 5 seconds grace period");
+                server.shutdown(5, TimeUnit.SECONDS);
+            }
+        });
     }
 }
--- src/main/java/HelloWorldRequestHandler.java
+++ src/main/java/HelloWorldRequestHandler.java
@@ -1,14 +1,24 @@
-import org.apache.http.*;
+import org.apache.http.HttpException;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
+import org.apache.http.nio.protocol.*;
 import org.apache.http.protocol.HttpContext;
-import org.apache.http.protocol.HttpRequestHandler;
 
 import java.io.IOException;
 
-public class HelloWorldRequestHandler implements HttpRequestHandler {
+public class HelloWorldRequestHandler implements HttpAsyncRequestHandler<HttpRequest> {
     @Override
-    public void handle(HttpRequest request, HttpResponse response, HttpContext context) throws HttpException, IOException {
+    public HttpAsyncRequestConsumer<HttpRequest> processRequest(HttpRequest request, HttpContext context) throws HttpException, IOException {
+        // Buffer request content in memory for simplicity
+        return new BasicAsyncRequestConsumer();
+    }
+
+    @Override
+    public void handle(HttpRequest data, HttpAsyncExchange httpExchange, HttpContext context) throws HttpException, IOException {
+        HttpResponse response = httpExchange.getResponse();
         response.setStatusCode(HttpStatus.SC_OK);
         String dbTestResult;
         try {
@@ -17,8 +27,9 @@ public class HelloWorldRequestHandler implements HttpRequestHandler {
             dbTestResult = e.toString();
         }
         StringEntity body = new StringEntity(
-            "<html><body><h1>Hello World OwO</h1><h2>"+dbTestResult+"</h2></body></html>",
-            ContentType.create("text/html", "UTF-8"));
+                "<html><body><h1>Hello World OwO</h1><h2>"+dbTestResult+"</h2></body></html>",
+                ContentType.create("text/html", "UTF-8"));
         response.setEntity(body);
+        httpExchange.submitResponse(new BasicAsyncResponseProducer(response));
     }
 }
